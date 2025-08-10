e476b98d1801217fc24a932f2d0fbc9ee43ca6b4
dhanus22 <smsaidhanush153@gmail.com>
2025-08-10 17:46:50 +0530

    changes to frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true});
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'https://clinic-front-desk-kk2505x1g-saidhanus-projects.vercel.app/login',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
